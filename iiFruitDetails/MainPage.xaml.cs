e0fe2477543cb65f6afa5ee69daac2d8ca3f20fe
Isaac Ingles <isaac.ingles@gmail.com>
2020-10-06 15:44:25 -0600

    Add project files.

---
+++ iiFruitDetails/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace iiFruitDetails
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        ///  Get the fruit name and display the color, type, and price
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PckFruit_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Store the selected item in a string - parse the object into a string with ToString()
+
+            // Check to make sure that there is an item selected or it will throw an error
+            
+            if (PckFruit.SelectedIndex != -1)
+            {
+                string fruit = PckFruit.SelectedItem.ToString();
+
+                // Switch statment - test the value of the fruit string
+                switch (fruit)
+                {
+                    case "Apple":
+                        // Logic for apple
+                        LblColor.Text = "Red";
+                        LblPlantType.Text = "Tree";
+                        LblPrice.Text = 1.99m.ToString("c");
+                        DisplayAlert("Apple", "An apple a day...", "Close");
+                        break;
+                    case "Canteloupe":
+                        // Logic for canteloupe
+                        LblColor.Text = "Tan";
+                        LblPlantType.Text = "Vine";
+                        LblPrice.Text = 0.99m.ToString("c");
+                        DisplayAlert("Canteloupe", "Cantelope is yummy.", "Close");
+                        break;
+                    case "Watermelon":
+                        // Logic for watermelon
+                        LblColor.Text = "Green";
+                        LblPlantType.Text = "Vine";
+                        LblPrice.Text = 0.69m.ToString("c");
+                        DisplayAlert("Watermelon", "I love watermelon.", "Close");
+                        break;
+                    case "Peach":
+                        // Logic for peach
+                        LblColor.Text = "Peach";
+                        LblPlantType.Text = "Tree";
+                        LblPrice.Text = 2.99m.ToString("c");
+                        DisplayAlert("Peach", "Peaches are gross..", "Close");
+                        break;
+                    default:
+                        // Display an error alert as the default response
+                        DisplayAlert("Error", "An error occured.", "Close");
+                        break;
+                }
+            }            
+                     
+            
+        }
+
+        /// <summary>
+        ///  Clear the labels/picker
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnClear_Clicked(object sender, EventArgs e)
+        {
+            // Reset the labels
+            LblPrice.Text = "Results...";
+            LblPlantType.Text = "Results...";
+            LblColor.Text = "Results...";
+
+            // Clear the picker by setting the index to -1
+            PckFruit.SelectedIndex = -1;
+
+        }
+
+
+    }
+}
